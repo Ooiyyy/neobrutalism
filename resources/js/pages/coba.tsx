b2094d9c824995a4d9a9e6de290754983d6fdccb
Ooiyyy <oiypes59@gmail.com>
2026-02-11 16:00:18 +0700

    belajar handle props

--- resources/js/pages/coba.tsx
+++ resources/js/pages/coba.tsx
@@ -1,23 +1,53 @@
 import { Card, CardContent } from "@/components/ui/brutal/card";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/brutal/carousel";
+import usePage from "node_modules/@inertiajs/react/types/usePage";
 
+type Coba = {
+    gambar1: string;
+    judul: string;
+    carousel: string[];
+    kartuUcapan: string;
+    gambarMomen: string[];
+}
 
-export default function Coba() {
+export default function Coba({ gambar1, judul, carousel, kartuUcapan, gambarMomen }: Coba) {
+
     return (
         <div className="w-full flex-col items-center gap-4 flex">
-            <Carousel className="w-full max-w-[200px]">
+            <img src={gambar1} alt="gambar1" />
+            <h1>{judul}</h1>
+            <Carousel className="w-100">
                 <CarouselContent>
-                    {Array.from({ length: 5 }).map((_, index) => (
-                        <CarouselItem key={index}>
+                    {carousel.map((urlDariprops, urutan) =>
+                        <CarouselItem key={urutan}>
                             <div className="p-[10px]">
                                 <Card className="shadow-none p-0 bg-main text-main-foreground">
                                     <CardContent className="flex aspect-square items-center justify-center p-4">
-                                        <span className="text-3xl font-base">{index + 1}</span>
+                                        <img src={urlDariprops} alt={`carousel-${urutan}`} className="w-full h-50 object-cover"
+                                        />                                    </CardContent>
+                                </Card>
+                            </div>
+                        </CarouselItem>
+                    )}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+            <p>{kartuUcapan}</p>
+            <Carousel className="w-100">
+                <CarouselContent>
+                    {gambarMomen.map((urlDariprops, urutan) =>
+                        <CarouselItem key={urutan}>
+                            <div className="p-[10px]">
+                                <Card className="shadow-none p-0 bg-main text-main-foreground">
+                                    <CardContent className="flex aspect-square items-center justify-center p-4">
+                                        <img src={urlDariprops} alt={`carousel-${urutan}`} className="w-full h-50 object-cover"
+                                        />
                                     </CardContent>
                                 </Card>
                             </div>
                         </CarouselItem>
-                    ))}
+                    )}
                 </CarouselContent>
                 <CarouselPrevious />
                 <CarouselNext />
--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,30 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/coba', function () {
-    return Inertia::render('coba');
+    $gambar1 = asset('gambar/coba1.jpg');
+    $judul = 'Coba Page';
+    $carousel = [
+        asset('gambar/coba2.jpeg'),
+        asset('gambar/coba3.jpg'),
+        asset('gambar/coba4.jpg'),
+    ];
+    $kartuUcapan = "Selamat!";
+    $gambarMomen = [
+        asset('gambar/coba2.jpeg'),
+        asset('gambar/coba4.jpg'),
+        asset('gambar/coba5.jpeg'),
+    ];
+    return Inertia::render('coba', [
+        'gambar1' => $gambar1,
+        'judul' => $judul,
+        'carousel' => $carousel,
+        'kartuUcapan' => $kartuUcapan,
+        'gambarMomen' => $gambarMomen,
+    ]);
 })->name('coba');
 
 Route::get('dashboard', function () {
     return Inertia::render('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
